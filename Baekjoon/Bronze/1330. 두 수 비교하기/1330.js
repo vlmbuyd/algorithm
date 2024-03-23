949fa98f1884b80ad8b2c184b6fc0d81b3327b21
Yean <kjan0929@naver.com>
2024-03-23 15:43:23 +0900

    [Bronze Ⅴ] 1330번. 두 수 비교하기

---
+++ Baekjoon/Bronze/1330. 두 수 비교하기/1330.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('dev/stdin', 'utf-8').split(' ').map(item => Number(item));
+
+
+if(input[0] > input[1]) {
+    console.log('>');
+}else if(input[0] < input[1]) {
+    console.log('<');
+}else {
+    console.log('==');
+}
