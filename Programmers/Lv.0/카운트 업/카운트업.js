d8767eb30f9ccba36ad562a66b552969365c8709
Yean <kjan0929@naver.com>
2024-06-27 11:16:03 +0900

    [Lv.0] 카운트 업

---
+++ Programmers/Lv.0/카운트 업/카운트업.js
@@ -0,0 +1,22 @@
+//풀이1
+function solution(start_num, end_num) {
+    let answer = [];
+    for(let i = 0; i < end_num - start_num + 1; i++) {
+        answer.push(start_num + i);
+    }
+    return answer;
+}
+
+//풀이2
+function solution(start_num, end_num) {
+    let answer = [];
+    for(let i = start_num; i <= end_num; i++) {
+        answer.push(i);
+    }
+    return answer;
+}
+
+//풀이3
+function solution(start, end) {
+    return Array.from({length: end - start + 1}, () => start++);
+}
