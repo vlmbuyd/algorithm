8ac6e0e23dbd430a44314fb4041eeed1a60aea5f
Yean <kjan0929@naver.com>
2024-08-01 10:31:58 +0900

    [Lv.1] 두 정수 사이의 합

---
+++ Programmers/Lv.1/두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+  const max = Math.max(a, b);
+  const min = Math.min(a, b);
+  let sum = 0;
+
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
