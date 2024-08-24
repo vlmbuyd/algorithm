b301e266450abc3fb816ac454d6e1ea7ca1c314e
Yean <kjan0929@naver.com>
2024-08-24 18:46:26 +0900

    [Lv.0] 수 조작하기 2

---
+++ Programmers/Lv.0/수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,10 @@
+function solution(numLog) {
+    let answer = '';
+    for(let i = 0; i < numLog.length - 1; i++) {
+        if(numLog[i + 1] - numLog[i] === 1) answer += 'w';
+        else if (numLog[i + 1] - numLog[i] === -1) answer += 's';
+        else if (numLog[i + 1] - numLog[i] === 10) answer += 'd';
+        else answer += 'a';
+    }
+    return answer;
+}
