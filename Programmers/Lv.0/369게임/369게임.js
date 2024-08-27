1fc714b99a3ae5d9c012a2e38fa979a85d07b578
Yean <kjan0929@naver.com>
2024-08-27 22:08:04 +0900

    [Lv.0] 369게임

--- Programmers/Lv.0/369게임/369게임.js
+++ Programmers/Lv.0/369게임/369게임.js
@@ -1,3 +1,4 @@
 function solution(order) {
-    return [...String(order)].filter(v => v === '3' || v === '6' || v === '9').length;
+  return [...String(order)].filter((v) => v === "3" || v === "6" || v === "9")
+    .length;
 }
