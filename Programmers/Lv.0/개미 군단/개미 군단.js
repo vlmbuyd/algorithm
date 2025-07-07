b81c776c21fecf486bb13f5ddf3c28337cf87652
김예안 <kjan0929@naver.com>
2025-07-07 20:54:29 +0900

    [Lv.0] 개미 군단

--- Programmers/Lv.0/개미 군단/개미 군단.js
+++ Programmers/Lv.0/개미 군단/개미 군단.js
@@ -1,10 +1,34 @@
+// 풀이 1
 function solution(hp) {
-    let result = 0;
-    for (let i = 5; i > 0; i-=2) {
-        if (i <= hp) {
-            result += Math.floor(hp / i);
-            hp = hp % i;
-        }
+  let result = 0;
+  for (let i = 5; i > 0; i -= 2) {
+    if (i <= hp) {
+      result += Math.floor(hp / i);
+      hp = hp % i;
     }
-    return result;
+  }
+  return result;
+}
+
+// 풀이 2
+function solution(hp) {
+  let answer = 0;
+  const GENERAL = 5;
+  const SOILDER = 3;
+  const WORK = 1;
+
+  while (hp > 0) {
+    if (hp >= GENERAL) {
+      answer += Math.floor(hp / GENERAL);
+      hp = hp % GENERAL;
+    } else if (hp >= SOILDER) {
+      answer += Math.floor(hp / SOILDER);
+      hp = hp % SOILDER;
+    } else if (hp >= WORK) {
+      answer += Math.floor(hp / WORK);
+      hp = hp % WORK;
+    }
+  }
+
+  return answer;
 }
