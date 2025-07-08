41aaf8186889efc0745cb6df964afb35dc9cc9bf
김예안 <kjan0929@naver.com>
2025-07-08 22:13:27 +0900

    [Lv.0] 짝수는 싫어요

--- Programmers/Lv.0/짝수는 싫어요/짝수는 싫어요.js
+++ Programmers/Lv.0/짝수는 싫어요/짝수는 싫어요.js
@@ -1,6 +1,18 @@
+// 풀이 1
 function solution(n) {
-    const answer = [...new Array(n)]
+  const answer = [...new Array(n)]
     .map((_, i) => i + 1)
-    .filter(item => item % 2 !== 0);
-    return answer;
+    .filter((item) => item % 2 !== 0);
+  return answer;
+}
+
+// 풀이 2
+function solution(n) {
+  const answer = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 1) answer.push(i);
+  }
+
+  return answer;
 }
