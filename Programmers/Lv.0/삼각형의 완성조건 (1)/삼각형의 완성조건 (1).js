de32efa6fa7f3abde1482fe7b78c4c848c9bd1aa
김예안 <kjan0929@naver.com>
2025-07-16 19:56:04 +0900

    [Lv.0] 삼각형의 완성조건 (1)

--- Programmers/Lv.0/삼각형의 완성조건 (1)/삼각형의 완성조건 (1).js
+++ Programmers/Lv.0/삼각형의 완성조건 (1)/삼각형의 완성조건 (1).js
@@ -1,6 +1,16 @@
+// 풀이 1
 function solution(sides) {
   const max = Math.max(...sides);
   const max_idx = sides.indexOf(max);
   const arr = sides.filter((_, idx) => idx !== max_idx);
   return max < arr[0] + arr[1] ? 1 : 2;
+}
+
+// 풀이 2
+function solution(sides) {
+  const max = Math.max(...sides);
+  const maxIdx = sides.indexOf(max);
+  sides.splice(maxIdx, 1);
+
+  return sides[0] + sides[1] > max ? 1 : 2;
 }
