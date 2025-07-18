945023f4ef0bd8033f915844152850785d845596
김예안 <kjan0929@naver.com>
2025-07-18 16:19:05 +0900

    [Lv.0] 배열의 원소만큼 추가하기

--- Programmers/Lv.0/배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
+++ Programmers/Lv.0/배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -1,7 +1,21 @@
+// 풀이 1
 function solution(arr) {
   const answer = [];
   for (const el of arr) {
     answer.push(...Array(el).fill(el));
   }
   return answer;
+}
+
+// 풀이 2
+function solution(arr) {
+  const X = [];
+
+  arr.forEach((el) => {
+    for (let i = 0; i < el; i++) {
+      X.push(el);
+    }
+  });
+
+  return X;
 }
