dec1d1cf725bb094bd2e2d83d1f2ea25159baf4a
김예안 <kjan0929@naver.com>
2025-07-19 21:54:41 +0900

    [Lv.0] 0 떼기

--- Programmers/Lv.0/0 떼기/0 떼기.js
+++ Programmers/Lv.0/0 떼기/0 떼기.js
@@ -1,15 +1,30 @@
+// 풀이 1
 function includesOneToNine(str) {
-    const regex = /[1-9]/;
-    return regex.test(str);
+  const regex = /[1-9]/;
+  return regex.test(str);
 }
 
 function solution(str) {
-    let answer = '';
-    
-    for (const el of str) {
-        if ((el !== '0') || (el === '0' && includesOneToNine(answer))) {
-            answer += el;
-        }
+  let answer = "";
+
+  for (const el of str) {
+    if (el !== "0" || (el === "0" && includesOneToNine(answer))) {
+      answer += el;
     }
-    return answer;
+  }
+  return answer;
+}
+
+// 풀이 2
+function solution(str) {
+  let idx = -1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "0") {
+      idx = i;
+      continue;
+    } else if (str[i] !== "0") break;
+  }
+
+  return idx === -1 ? str : str.slice(idx + 1);
 }
